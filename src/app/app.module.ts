4dae8096c16328388369ddf57012915af3df78da
Muthu Aiswaryaa Swaminathan <withlove.aish@gmail.com>
2021-06-20 12:54:44 +0530

    Login page, Google Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import KEY from './../environments/config.js'
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import {AuthGuardService} from './auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'mainpage', component: MainPageComponent},
+      {path: '**', component: LoginComponent}
+    ]),
+    BrowserAnimationsModule,
+    SocialLoginModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(KEY)
+        }
+      ]
+    } as SocialAuthServiceConfig,
+  },
+    //AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
